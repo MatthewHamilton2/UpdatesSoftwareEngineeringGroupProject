4b62c40423a4ca1afcb7e0b797af83aad9e9b12e
MatthewHamilton2 <99368699+MatthewHamilton2@users.noreply.github.com>
2024-02-01 12:39:30 +0000

    Add files via upload

--- login.php
+++ login.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body class = "unset">
     <header>
 		<div class="logo">
 		  <img src="placeHolder.PNG" alt="Collab Nexus Logo">
